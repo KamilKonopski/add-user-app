4928662a90791170f757b4d225f22a86804e058e
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-21 16:09:50 +0200

    add simple validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -11,7 +11,17 @@ const AddUser = () => {
 
 	const addUserHandler = (event) => {
 		event.preventDefault();
+
+		if (userName.trim().length === 0 || age.trim().length === 0) {
+			return;
+		}
+		if (+age < 1) {
+			return;
+		}
 		console.log(userName, age);
+
+		setUserName("");
+		setAge("");
 	};
 
 	const userNameChangeHandler = (event) => {
@@ -26,9 +36,14 @@ const AddUser = () => {
 		<Card className={classes.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor="username">Username</label>
-				<input id="username" type="text" onChange={userNameChangeHandler} />
+				<input
+					id="username"
+					type="text"
+					value={userName}
+					onChange={userNameChangeHandler}
+				/>
 				<label htmlFor="age">Age (Years)</label>
-				<input id="age" type="number" onChange={ageChangeHandler} />
+				<input id="age" type="number" value={age} onChange={ageChangeHandler} />
 				<Button type="submit">Add User</Button>
 			</form>
 		</Card>
