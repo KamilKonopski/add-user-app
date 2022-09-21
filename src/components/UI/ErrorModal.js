67671b928ff418aa7cd506e54d915082e388fa1b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-21 17:09:17 +0200

    add react portal to add error modal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
 import Button from "./Button";
 import Card from "./Card";
 
 import classes from "./ErrorModal.module.css";
 
+const Backdrop = ({ onConfirm }) => {
+	return <div className={classes.backdrop} onClick={onConfirm} />;
+};
+
+const Modal = ({ title, message, onConfirm }) => {
+	return (
+		<Card className={classes.modal}>
+			<header className={classes.header}>
+				<h2>{title}</h2>
+			</header>
+			<div>
+				<p className={classes.content}>{message}</p>
+			</div>
+			<footer className={classes.actions}>
+				<Button onClick={onConfirm}>Okay</Button>
+			</footer>
+		</Card>
+	);
+};
+
 const ErrorModal = ({ title, message, onConfirm }) => {
 	return (
-		<div>
-			<div className={classes.backdrop} onClick={onConfirm} />
-			<Card className={classes.modal}>
-				<header className={classes.header}>
-					<h2>{title}</h2>
-				</header>
-				<div>
-					<p className={classes.content}>{message}</p>
-				</div>
-				<footer className={classes.actions}>
-					<Button onClick={onConfirm}>Okay</Button>
-				</footer>
-			</Card>
-		</div>
+		<Fragment>
+			{ReactDOM.createPortal(
+				<Backdrop onConfirm={onConfirm} />,
+				document.getElementById("backdrop")
+			)}
+			{ReactDOM.createPortal(
+				<Modal title={title} message={message} onConfirm={onConfirm} />,
+				document.getElementById("modal")
+			)}
+		</Fragment>
 	);
 };
 
